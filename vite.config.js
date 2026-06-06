000ef3116f9b67934764ac6973d39f4040d5a5ac
tirumalateja19 <tirumalateja.jampani@gmail.com>
2026-06-06 10:00:33 +0530

    google site-map

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
+import sitemap from "vite-plugin-sitemap"
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    sitemap({
+      hostname: "https://www.politechsolutions.com",
+    }),
+  ],
 });
